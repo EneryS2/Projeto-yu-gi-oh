33d8dfcbe4f164f3ca4ea11021261fc00a7718ad
EneryS2 <leorenesantos@gmail.com>
2025-01-26 01:39:07 +0000

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -16,5 +16,34 @@ anterior da lista
 */
 
 // passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
-const btnAvançar = document.getElementById(btn-avancar);
-const
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao(cartaoAtual) {
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
